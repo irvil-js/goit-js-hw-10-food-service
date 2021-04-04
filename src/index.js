3d50ac288d56fc8c226462007a4ccdb0ada44e67
Irina Murashka <irvil@tut.by>
2021-04-04 18:21:32 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import './js/theme.js';
+import menuElement from './menu.json';
+import menuTemplate from './templates/menu-template.hbs';
+
+
+let menu = document.querySelector('.js-menu');
+
+function markUpMenu(array) {
+    const markup = menuTemplate(array);
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+markUpMenu(menuElement);
